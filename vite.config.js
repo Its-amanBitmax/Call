1cd84b55283caa6c961fe999d0e4bba51622d5fb
Its-amanBitmax <bitmax.aman@gmail.com>
2025-08-28 15:02:12 +0530

    hi

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: 'localhost'
-        }
-    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx', 'resources/js/Pages/welcome.jsx'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx', 'resources/js/Pages/Welcome.jsx', 'resources/js/Pages/Contacts.jsx'],
             refresh: true,
         }),
         react(),
